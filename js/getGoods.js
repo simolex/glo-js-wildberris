2bfe90196ba0a77c84d5dae8a43c004a5c0a6232
simolex <simolex@yandex.ru>
2021-11-21 23:20:05 +0300

    third lesson+

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,20 +1,65 @@
 const getGoods = () => {
   const links = document.querySelectorAll(".navigation-link");
+  const buttonViewAll = document.querySelector(".more");
 
-  const getData = () => {
+  buttonViewAll.addEventListener("click", () => {
+    getData();
+  });
+
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector(".long-goods-list");
+
+    goodsContainer.innerHTML = "";
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+      goodBlock.innerHTML = `
+            <div class="goods-card">
+              <span class="label ${good.label ? null : "d-none"}">${good.label}</span>
+              <img
+                src="db/${good.img}"
+                alt="image: ${good.name}"
+                class="goods-image"
+              />
+              <h3 class="goods-title">${good.name}</h3>
+              <p class="goods-description">${good.description}</p>
+              <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                <span class="button-price">$${good.price}</span>
+              </button>
+            </div>
+      `;
+      goodsContainer.append(goodBlock);
+    });
+  };
+
+  const getData = (field, value) => {
     fetch("db/db.json")
       .then((response) => response.json())
       .then((data) => {
-        localStorage.setItem("goods", JSON.stringify({ name: data }));
+        const array = field ? data.filter((item) => item[field] === value) : data;
+
+        localStorage.setItem("goods", JSON.stringify(array));
+        if (window.location.pathname !== "/goods.html") {
+          window.location.href = "/goods.html";
+        } else {
+          renderGoods(array);
+        }
       });
   };
 
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
       e.preventDefault();
-      getData();
+      const linkValue = link.textContent;
+      const linkField = link.dataset.field;
+      getData(linkField, linkValue);
     });
   });
+  if (localStorage.getItem("goods") && window.location.pathname === "/goods.html") {
+    renderGoods(JSON.parse(localStorage.getItem("goods")));
+  }
 };
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -1,14 +1,58 @@
 const search = () => {
   const inputSearch = document.querySelector(".search-block > input");
   const buttonSearch = document.querySelector(".search-block > button");
-  let valueInput;
 
-  inputSearch.addEventListener("input", (event) => {
-    valueInput = event.target.value;
-  });
-  buttonSearch.addEventListener("click", () => {
-    console.log(valueInput);
-  });
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector(".long-goods-list");
+
+    goodsContainer.innerHTML = "";
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+      goodBlock.innerHTML = `
+            <div class="goods-card">
+              <span class="label ${good.label ? null : "d-none"}">${good.label}</span>
+              <img
+                src="db/${good.img}"
+                alt="image: ${good.name}"
+                class="goods-image"
+              />
+              <h3 class="goods-title">${good.name}</h3>
+              <p class="goods-description">${good.description}</p>
+              <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                <span class="button-price">$${good.price}</span>
+              </button>
+            </div>
+      `;
+      goodsContainer.append(goodBlock);
+    });
+  };
+
+  const getData = (value) => {
+    fetch("db/db.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const array = data.filter((good) => good.name.toLowerCase().includes(value.toLowerCase()));
+
+        localStorage.setItem("goods", JSON.stringify(array));
+
+        if (window.location.pathname !== "/goods.html") {
+          window.location.href = "/goods.html";
+        } else {
+          renderGoods(array);
+        }
+      });
+  };
+
+  try {
+    buttonSearch.addEventListener("click", () => {
+      getData(inputSearch.value);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 search();
